501c9d6ffdbaa8b219952fe486d75ffaa4533ae3
Boyan Liu <heyiamboyan@gmail.com>
2019-09-25 09:47:13 +0800

    Add log in/ sign up  page

---
+++ public/js/login.js
@@ -0,0 +1,20 @@
+console.clear();
+
+const loginBtn = document.getElementById('login');
+const signupBtn = document.getElementById('signup');
+
+loginBtn.addEventListener('click', (e) => {
+	let parent = e.target.parentNode.parentNode;
+	Array.from(e.target.parentNode.parentNode.classList).find(() => {
+		signupBtn.parentNode.classList.add('slide-up')
+		parent.classList.remove('slide-up')
+	});
+});
+
+signupBtn.addEventListener('click', (e) => {
+	let parent = e.target.parentNode;
+	Array.from(e.target.parentNode.classList).find(() => {
+			loginBtn.parentNode.parentNode.classList.add('slide-up')
+			parent.classList.remove('slide-up')
+	});
+});
